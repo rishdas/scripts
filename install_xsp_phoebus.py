49cb009a0d8a342e815074091b0e695b253fd238
rishdas <sirshakdas@gmail.com>
2016-02-04 16:40:10 -0500

    install script

---
+++ install_xsp_phoebus.py
@@ -0,0 +1,10 @@
+import subprocess
+subprocess.call(["git", "clone", "https://github.iu.edu/sirdas/crest_share_files.git"])
+subprocess.call(["sudo", "yum", "install", "libxsp-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "libxsp-client-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "libxsp-debuginfo-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "libxsp-xspd-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "phoebus-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "phoebus-client-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "phoebus-debuginfo-1.0-10.x86_64.rpm"])
+subprocess.call(["sudo", "yum", "install", "phoebus-server-1.0-10.x86_64.rpm"])
